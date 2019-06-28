2832f3f4df4cc38677a59acac16730be79f93676
Marco <marcodallatiezza@gmail.com>
2019-06-28 16:27:48 +0200

    Introduced log and minor fix to Windows deploy script

--- deploy/Windows/deploy_win.bat
+++ deploy/Windows/deploy_win.bat
@@ -1,4 +1,8 @@
 @echo off
+echo    ===================================
+echo          Artemis 3 Deploy Script
+echo                 WINDOWS
+echo    ===================================
 REM  Check and gain admin permissions
     IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
 >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
@@ -23,23 +27,30 @@ if '%errorlevel%' NEQ '0' (
 :gotAdmin
     pushd "%CD%"
     CD /D "%~dp0"
+    echo:
 
 REM Set the correct permissions for Artemis folder
 set artemis_path=%~dp0..\..
-icacls "%artemis_path%" /grant %USERNAME%:(OI)(CI)F /T > nul
+icacls "%artemis_path%" /grant %USERNAME%:(OI)(CI)F /T > log
+echo Gaining admin privileges and set folder read/write permission... DONE!
 
 REM Download necessary libraries with pip3
-set choice=Y
-set /p choice=Download necessary Python libraries? [Y]/N		
-if /I '%choice%'=='Y' pip3 install -r %~dp0requirements_win.txt
 echo:
+set choice=Y
+set /p choice=Install the necessary Python libraries? [Y,N]...
+echo:
+if /I '%choice%'=='Y' pip3 install -r %~dp0requirements_win.txt --no-color >> log
 
 REM Generation of shortcut
+echo:
 set choice=Y
-set /p choice=Create a desktop shortcut? [Y]/N		
+set /p choice=Create a desktop shortcut? [Y/N]...	
 if /I '%choice%'=='N' goto end
 
-ren "%artemis_path%\Artemis.py" "Artemis.pyw"
+IF EXIST "%artemis_path%\Artemis.py" (
+    ren "%artemis_path%\Artemis.py" "Artemis.pyw" 
+)
+
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
 echo sLinkFile = "%USERPROFILE%\Desktop\Artemis.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
@@ -51,6 +62,8 @@ cscript /nologo CreateShortcut.vbs
 del CreateShortcut.vbs
 :end
 echo:
-echo SETTING COMPLETE!
+echo    ================================
+echo           SETTING COMPLETE    
+echo    ================================
 echo:
 pause
