54f31a9b6a14409d80e76c216610f53ba51ecb83
Marco <marcodallatiezza@gmail.com>
2019-06-28 08:55:59 +0200

    Introduced log and minor fix to linux deploy script

--- deploy/Linux/deploy_linux.sh
+++ deploy/Linux/deploy_linux.sh
@@ -1,14 +1,24 @@
 clear
+echo "
+    ===================================
+          Artemis 3 Deploy Script
+                  LINUX
+    ===================================
+"
+
 # Set the correct permissions for Artemis folder
+echo "Gaining admin privileges and set folder read/write permission..."
+echo ""
 sudo chmod 700 $PWD/../../
 # Download necessary libraries with pip3
-read -p "Install the necessary Python libraries? [Y,N]      " doit 
+read -p "Install the necessary Python libraries? [Y,N]..." doit 
 case $doit in  
-  y|Y) pip3 install -r requirements_lin.txt --user ;; 
+  y|Y) pip3 install -r requirements_lin.txt --user > log;; 
 esac
 
 # Generation of shortcut
-read -p "Create a desktop shortcut? [Y/N]      " doit 
+echo ""
+read -p "Create a desktop shortcut? [Y/N]..." doit 
 case $doit in  
   y|Y)
 cat << EOR > /home/$USER/.local/share/applications/artemis.desktop
@@ -22,9 +32,16 @@ Icon=artemis3
 Type=Application
 EOR
 sudo cp ./artemis3.svg /usr/share/icons/
+echo "
+Link copied in: /home/$USER/.local/share/applications/artemis.desktop
+Icon copied in: /usr/share/icons/artemis3.svg
+"
   ;;
   n|N) ;;
   *) echo "invalid option $REPLY";;
 esac
-echo ""
-echo "SETTING COMPLETE!"
+echo "
+    ================================
+           SETTING COMPLETE    
+    ================================
+"
