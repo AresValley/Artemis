b1250c06c7c529fd62ff2c029b02c5141b6c049c
Alessandro <alessandro.ceccato.3@gmail.com>
2019-09-21 22:40:01 +0200

    Fix spec_files/Linux/build.sh

--- spec_files/Linux/build.sh
+++ spec_files/Linux/build.sh
@@ -3,29 +3,32 @@ echo "Build Artemis executable.."
 rm -rf output
 
 mkdir output
+mkdir output/artemis
 
-pyinstaller artemis.spec
+pyinstaller Artemis.spec
 
-mv -v dist/Artemis ./output/Artemis
+mv -v ./dist/Artemis ./output/Artemis
 rm -rfv dist build
 
 echo "Build _ArtemisUpdater.."
 
-pyinstaller _ArtemisUpdater.spec
+pyinstaller updater.spec
 
-mv -v dist/_ArtemisUpdater ./output/_ArtemisUpdater
+mv -v ./dist/_ArtemisUpdater ./output/_ArtemisUpdater
 rm -rfv dist build
 
 echo "Create single archives"
 cd output
-tar -czvf Artemis_linux.tar.gz Artemis ../../../src/themes
-tar -czvf _ArtemisUpdater_linux.tar.gz _ArtemisUpdater
+
+tar -czvf Artemis_linux.tar.gz ./Artemis ../../../src/themes
+tar -czvf _ArtemisUpdater_linux.tar.gz ./_ArtemisUpdater
 
 echo "Create full archive for website"
-mkdir Artemis/
-cp -rv !(__current_theme) Artemis ../../../src/themes _ArtemisUpdater Artemis/
+
+cp -rv Artemis _ArtemisUpdater ../../../src/themes artemis
+rm -f artemis/themes/__current_theme
 # rm Artemis/themes/__current_theme
-tar -czvf ArtemisWebDownlaod_linux.tar.gz Artemis/
+tar -czvf ArtemisWebDownlaod_linux.tar.gz artemis/
 
 echo "Get size and sha256"
 python ../../__get_hash_code.py Artemis_linux.tar.gz _ArtemisUpdater_linux.tar.gz ArtemisWebDownlaod_linux.tar.gz
