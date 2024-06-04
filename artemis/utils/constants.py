AresValley/Artemis

commit 10607c88ea505e1fd4fa2160aaffa7c324ea9c61

Searches in the name field has been extended  to the description as well (close #44)

artemis/utils/constants.py
@@ -59,7 +59,7 @@ class Query():
     
 ############################## SELECT
 
-    SELECT_ALL_SIGNALS = "SELECT SIG_ID, NAME FROM signals ORDER BY NAME ASC"
+    SELECT_ALL_SIGNALS = "SELECT SIG_ID, NAME, DESCRIPTION FROM signals ORDER BY NAME ASC"
     
     SELECT_ALL_MODULATION = "SELECT DISTINCT VALUE FROM modulation ORDER BY VALUE ASC"
     

artemis/utils/sql_utils.py
@@ -96,7 +96,7 @@ class ArtemisDatabase(Database):
             contains the SIG_ID and the NAME of the signal
         """
         self.all_signals = self.execute(Query.SELECT_ALL_SIGNALS)
-        keys = ('SIG_ID', 'name')
+        keys = ('SIG_ID', 'name', 'description')
         result = [dict(zip(keys, values)) for values in self.all_signals]
         self.all_signals = result
 

artemis/resources.py
@@ -5108,167 +5108,169 @@ z7\x80\xec$\xf4c9\xf2\xd1\x8f4\xc6\xbc>\xb8\
 \x116\x9d`\x1e\x97*W\xd9M\xe1\x09\x12\xa5\xfc\xda\
 h;Q\xfc\xeb\x9c\x87 J\xfd\xc5\xd0c0\xc6[\
 \xf7\xe4\x19\xd3~\xf5\xc8?\xf0\xff?E#\xf2\xe8\
-\x00\x00\x09\xec\
+\x00\x00\x0a\x02\
 \x00\
-\x001\x8ex\xda\xcd\x1ako\x1b7\xf2{\x81\xfe\x07\
-V\x1fj\xe9\xea[9\x06\x02\x14j}\x80-9\xb1\
-\x01\xdbq#7\xc1!\x08\x02j\x97\x92\x08S\xcb=\
-.\xd7\xb2\x9b\xea\xbf\xdf\x0c\xb9\xef\x97V\xb2s9%\
-\x90\xb5\xe4\x0c9/\xce\x8b\xcbW\x81T\x9a\xfc\xa1\xff\
-\x88\xb8{\xff\xe3\x0f\xbc\xf0\xec|\xe4\xbe'\xd7\x95\xe1\
-\xb1\xf4\xb5\x92\x22l\x9cp\xae\xa9f\x8aSQ\x81\xb8\
-\xa2O2\xd2U\xcc\x09\x00\xcb\x05\x8c\xe3?\xbb-\xf9\
-\xfa\xe3\x0f\x04>\xdc\x1b\x91uL\x08>\xaf\xb9\xa7\x97\
-#\xf2\xea\xd5\xd1\x91\x1dX2\xbeX\xea\x11\xf9\x15\x07\
-\xec\xd0X\xc2\xf2>\xf3\xb5#}\xfc-\x98f\xb0L\
-\xbc\x22~\x1e\xc9\x09\x99\xba\x8a1\xdf1\x0b\x92!9\
-&\xff$\xe9\xef\x0c\xf2)\x83\xb4;\xc5\xa0\xd9\x83\x85\
-\xdd$\x9bk\xae\x05\x1b\x91\xff\x84w\xaa\xdf;U\x9a\
-\xadx\xd8\x1b\xd8\xb9\x07\x1e\xf2\x19\xcej\x151;d\
-\xbfW\xd2\xa3\x82\xeb\xa7\x11\x88\xc49\x0d\x02\xc1]\xaa\
-\xb9\xf4\xafq\xdc\x82\xcc\x05]\x84f\xfecN\x1c\xf6\
-{8$v0\x04\x16\xf4\x12dL\xa2 `\x8a\xcc\
-\xa8J 2\xfc7\x8a\xae\x98`ahq.\xb8\xaf\
-\x13\xe2C\xbe\xf0\xa9 BRoj~\xf6a2\x1e\
-\xbd\xf4\x06\x05\xa0p)\xd7\xb7\x8a\xcd\xfb\xd5\xe1\xc9\xd9\
-\x8a\xfat\xc1T\xcd\xdc\x98\xea\xeb\xdaI\x190\x1f6\
-=\xf7\xb8\x96\xaa\x1fj\xc5\xfd\x05\xd1O\x01;$\x0f\
-T!\xd4\x97\x80\x02\xe9\x87d&\xa5 <\xfc\xe2\xb3\
-uu\xfdi@]\xf6\x91Q\xbd\xac\xec\xe0.\x99{\
-?\x99\xfd\x19x`\xa0a\x996M\x95\x9e\xc8\xb5\x8f\
-\xcc\xd7\x127\x99M\xb8b.P\xf7T\x9a\x05B&\
-\xb3\x84d\x1f\xa4[\x9cf\x8fh\xed\x19D\x00\xc4\x15\
-!\xecy\xa8@X\x98@\xc1\xeeJ?\x191\x18\xea\
-\xbc+\x1e\xa6:\x9bG\xbe\x8b\xb6B\x02\x19D\x028\
-\xc3\xc9\xbe]8\xc4\xdf\x83\xbc\xe5g\xf8`\xd89\xa0\
-\x0cB\xb3G\xfd\x863\xe1M\x19U\xee\xd2a>\x05\
-\x9b\xf5\x00<\xb3Zc\xcc@\x8d\x1b)\x05\xe7\xec\xd2\
-\xf7\x18\x1e)\x01\x0b}\xe0l\xed\xe4\xc73\x0c0\x16\
-\xc5\xc2\xa5\xa1o\x90\x0d\x83\xf5N\x99&\xa0\xb0\xe2z\
-3\xea\xde\x13:\x07_\x92`\x1a\x8b\x000Q \x98\
-\xcfI\xbf\x80\xf8\xaf\x13rD~\xfe\xb9\xb8\xda\xef\x06\
-\x0b\x0e\x14\x13\x8e+#\xbf(\x16#\x9a:\xea\x81\xab\
-zf6\xa5s\x9f\xaa\xa1\xc0eA\xf4\xd9\xfe\x02D\
-\x9b\x17\xc1\x5c*\xd2G\x81r\xd8\xef\xe87\xf8\xf3{\
-NS\x8e`\xfeB/a\xf8\x97_*T#\x16\x9a\
-\x1c\x8a?\xc5\xf8\xc4?;8\xe8hy%\xd7L\x8d\
-i\xc8\xf2\xfb%\x88\xa1Q1\xaa\xb6\xa4t|nC\
-F\x99\x9b\x0d\xb8\xef\x8a\xc8\x83\xd3d\x97\x1aT\xe8+\
-rN\xc1-\xf9^\xbf@ii\xe9ME\xc6fC\
-p\xa4\xe3%\xf5\x17\x16-\xa1\xa7*\xfe\x0a`\x9e \
-\xcb\xb3\x80C\xcc\xbc/`\xfd\xb1\xcdZ\xda\x16L\xf7\
-km`P\xb4\xb5\xc2\x02?\x9d\x9c\x90\x08\x80\xe6\xdc\
-g^\xd5\xa42G\xdae\x1fgz\xf9\xf6\xcb\xe5\xa4\
-$\x10\x06\x1e\xd1.r\xcd\xfc\xa8\xf1<n\x08\x13!\
-+S\xd0\x88<\x87\x83\xcf:\x98\xb3\xb1\xd5\x9d\x8c\xb9\
-\xe0b>}n\xf6,\x15\xccf\xd7\x93\xa3\xb6J\xa2\
-\x90\xee=r\xd7\xd7r\xb1\x10\xac@'*\xacf\x18\
-?\xe8\xd3\xdfp\xc1\xda\x05c\x84h\x1cx7X\x88\
-\x05]\xe4\x9d\xe8\x06\xc2![@D\xd9\xa2\x9dZ\xdd\
-60P4\x8bV\xfa\xab\xa0M\xe4\xd7,\xdaL}\
-\xc90\x9b\xf46\x93Z\xcb\xd5\xa5?\x97g`\x07+\
-\xc8H -8$\xf1\x8f;\x08\xfd\x05\x9de\xe0W\
-t\x06\x86\x87\xd6\x02\x9b\xc5\xe0\x19\x5c\x089\x108\xb5\
-~\xd3:.\xf83\xd2[S\xe5C<\xe9\x8d\x8a\x5c\
-\x957q\xa5\x00\xdf|B\x92\x9c\xd6\x0e\xf4\xd3\xc7\xf7\
-\xcc+\x1d\xd7\x99b\xf4\xbe\xbc\x1b\x87\x05w\xde\x0a\xa2\
-\x02[(\x88U^\xeb\x0e\x8d\xf2E\xfbx\xcb|\xa6\
-\xa8\xb0\x19v^$\x876SM\xc5]\x10\x91g\xc0\
-c\x5c'\x87\x95\xc9\x1b\x9f\x9a\x10\xcc\xcah\x07\xf8w\
-\xcb\xaau\x1a,\x02\x22\x1b\xcd\xfe\xdedf\x06>I\
-\xde \x8d\xda\x8d\xcf\x0c\xb1;\xab9\x9cVn\xabk\
-73\x9c\x83\xdd\x95\xe7\xb8\xd0\xd8\x93\xf1\x18{w\xee\
-\x13\xc4N\x22(\xed\xb2]\x0e\x09B\xad0,\xff\xd7\
-\xf1Zy\x8f\x0f\xa5^Y\x9e\xd9\xac\xa9\xb4\x92\x02,\
-\xe734\xf5=\xaa\xbc\xb3\x08\xce\xa4\x0fu\x92]\xde\
-\x19S\xdfe\x82\xfc\x9d<\xff\x9b\x85y4\xe9\x9f\xba\
-.\x0b\xca\xe5e\xbcb]=Qw`w`%\x16\
-\xc9w\xe0\x07\x0aG\xd4\xc3\x9fJ\x9c?\x82\x7f\x82\x08\
-!\x9e\xfa\xbd\xa5\xd6A8\x1a\x0e\x17PvF3p\
-a\xab\xe1)d\xbf\x1f`\x96=\x0dK\xe5\xef\x9e\xbc\
-\xc7~`o\x9e\xdf\xdd\xd7\xeeY\xbf\xd9\x0d\x96r\xf9\
-U\x1fG\xa4\x0fu'v\x13l{ n\x13\x0cJ\
-}\x82\x0c,n\x0d$=\x82\x18\xb0\x81\xfc4L\x08\
-\x19\xb2[\x09\x85?\xact\x0b\x95\x5c\xe0\xdc\xc8\xd3H\
-\xcb1Ntc3S\xabUs\x1ek,E\xb4\xf2\
-m\x03\xa6\xacZ\x00^J\x15:s.\x80.\xcbE\
-\x11\xc2\x04\xa8\xba\xcc\x1ecp\xd2\xec8\xf7\xb1T\xc3\
-\xfa\xcc\xd4#rn\x7f\xb35\x81z\x9b\xce \x0e\x8e\
-z\x83\xea\x12\x96&\xc7F\xc3k\xaa\x16\xdc\x1f\x91W\
-\xaf\xab\x80s\x09\xc2\x0d$\xf7!\xa1\xfd\x8b\x01\xccq\
-c\xe1\x80\x9f\xbb$\x99\xac\xa3\x1b\x15n\xaa\xf6\x1b \
-\xb5\x91&\x14\xc8G\xdbp\xaa\xe6?\xa64\x17\xd4e\
-K)\xe0\x80\xdf\xe5$\x81\x8b\xf6Z\xcb\x9aNG\xce\
-\xb6\x15R2M\xc2\xd3r\x940\xbf\xab7\xed\xb8\x01\
-a\xe6r\xd9\xb6\xedT\xf5n!\x0d\x87\x1c\x05l\x00\
-,\x8dP\x12\xd2\x07\x06\xc2F\xa6\x1c\xc7\xe9\xe5\xaaT\
-L !\xe7\x1f\xe5\xb6r\xa6\x00\x8d\x8f\x19\x18j\x1f\
-F\xc0\x18\xc08?\xf5N!\x93\xe7\xb0b\xdf\x01\x8f\
-8\xe8}\xee\xc8|\xd64\x89\xcb-\xdcd/\xee\xe3\
-\xe6JG\xee\x1f\xa8\xe0\x1e\x01R\x9d\xc5_\x84Z\xda\
-;\x88\xe1]\x9c\x8bo\x17\xc3?v\x93C\xd6\x1a\xea\
-&\x87\xd3Y\xe9\x84\xa3\x08(\x0e\xe6%\x90\x82\xdf\x96\
-\xdcn\xbb/\x10l\xaeo\xa9\xe7A\xe2<\x22\xb9#\
-\xaa\xd0\xd1\xd5M\xd8S\x9d\x9f\xc9\xe6\x96\x0c\x03\xe3\x88\
-`\xf1\x00\x05@\x85q\xa0[\xcb\xe0\x0c[\x98\xc5\x19\
-D\xa8uF\xf9\xd6+\x8a\xa97(\xa3&\xe8\x97\x10\
-\x98\xea\x96\xc8|Z\x0f\x22\x01\x99\xc4\x8e\xabh\x00\x85\
-\x1a\xcc\x1f\x83\xcf\xbe7\x8a\xcbE\x908g\xd9T\x91\
-6\xcf \xe9\x0a2\x80\x1di*\xb5cw!h\xca\
-\xb0\xdf\xaa\xa1&\xf9\xba\x17\xd1\xd9\xd9K*\xcfV\xe6\
-.\xed\x9d@\xc2^\x07\xde\xf2\x07\xfb\x85\x05\x9ey\xcd\
-N\xb4\x9f?\xeeJ{\xde%7\xd3n\x1c\xa1-\xab\
-Gu\x1d\x84o\xae\xbfr\x91\xdf.\x05\x00&wt\
-\x11v\xb4\xcb\xfcU\xc0\xff#\xf3\xf9\xfeJ+\xe3\xe8\
-\xfcS\xe5\x937&tv\x90A\xe5R\xe1\xa5\x85\xd0\
-\xc1\xa5\xe0\x0d\x0e\x03'\xef\xb2\xaeZ\xb3w>/\xec\
-\xd9\xce\x1f\xb9\xee\xb0\xbf\xbd\x09tL\xb2\xdc\x8d\x86M\
-\xd7\xe0\x81\x0a\x0f\xd3\xce\xd7\x96\xe0b[d\xfb\x85\x97\
-8\xf5\x9c\xb6\xec\xd5E\xe9\xc5H\xd5)\x16\x14o\xd8\
-\x0e,\x05\x07\x87\xe4\xd3\xe7C\x93\xdf\xbe\xac\xf7,4\
-\x9e\x9f\xcd#\x12\xed<\x9bK\xb3O\x03\x9b{YM\
-\xd1*\xf0\xea\x91\xc4w\x8f\xcf\xca=\xc6xSI\xde\
-3\x8c\x11\x1dX\xae\x870\xc5b\xc3\x8d\xe8\x16\xfe\x9f\
-#\x924\xdf\xecp\x8e.\x98\x08^@Px}\x16\
-\xdf\xeav\x90V\xf9\x1a\xf8\x7f\x99\x13\xc5dO\xa4\x1b\
-\xad \xb36\x97\xfc\x1dH\xaem\xbe\x1c$\xcd\x97\xac\
-\xe3\xe2\xc4}\x18.\x93\xde\xcb\xc1K{\xea)X\x14\
-X\xa6\xad\x9and7\x91\xb7\xd3\xdf\xda<\x1a\xce\x84\
-\x9c\x0dW4\x04\xbc\xe1\xf8\xe2\xf4\xe6\xed\xf9\xd5\xbb\xb7\
-\xce\xca;\xf8\x1e\x8a\xb3G\x92\x5c\x86a\xc4\xbe1\xdf\
-\x1c\xf7\x08\xbf\x0b\x97\xa6\x86|\x96\xdf\xc9\x15\x9c\x85\xf6\
-\xed^~\xa7\x96\xed\xb9\x04\xdb\x83\xea\xb1\xb65\x85>\
-\xa8t\xbdR\x04\xb0\xbd$\xfe\xc8D}/\xa9\xa9\xd0\
-m-v\xeb\x0b\xdeZ\xea\xdf\xcb\xf5\xbe\xbd\xb8\x10\xdc\
-\xb9Y\xfb\xf8\xa8\xac\xe2\xb6&_\xae\xad\xb5\xa2\x8f|\
-\x15\xad\xe2\xce\xd6\xf1\xeb\xa3:Sim\x9f\xa5\xf5y\
-\xf1\xc2v\xff(\xdf\xb5\xe1\x96\x83\xd52H\xbb\x85G\
-\xf5\x90\x0d\xed9Klo\xd0d\xe0\x08\x1b\xbfB\xd0\
-j\xe4\x0d\xef\xb3l\xb7\xef\xea`\xc1\x1e\x1a\xd8\x8e_\
-\x19h\xa3\x08u\x92\xbcZ\xd0\x0c\xb5\x8b\xb0K\xf0\x17\
-\xf1\xdbv\xed\x08K\x00\x12\x08x-\x1f\xd8$R&\
-\xcc\x8d\xc8Q3\x86+x\xb0m\xd59\x04\xcdp\x1b\
-\xd0\xd4UR\x883\xaa\x9c\x07\xa64w\xb1\xd3\x9e\xbe\
-\x05\xd7J\xeb\x08\xa2\x9a\xabA\xeb\x82\x91\xaf\xc4\x5c\xc6\
-\x8e\xb2\xbbX\xea\xbap\x12\x7f#\x8az\x1c\xa9xM\
-6\xcdk\x82\x8f\xcc\xbd\xd6\x91\x99R\xcd\xcb)-\xab\
-x\x10d\x17\xb0\xff\x88\xb4\xb9\xec\xb2\xea'1V;\
-t\xfc\x1aebS\xce\x03~\x99\xc1v\xbc\xe4m\xcb\
-\xe3\xa3v\xb8\xd8+\xdbx\x82\x9d\xd0M;\xfc\xb5\x8c\
-B\x06A\x90nc\xb3\x9b\x8f\xdc\x12\xb8\x9a\xde\xf4\xe2\
-\xc5W\xbc:\x1e\xe8\x0eS+|S\xc6\x8a\xdb\xbc4\
-\xd3U\x99\x06x\xe7\x1d7M.$=\x1c\xdb|H\
-\xeb\x89\xd9\xd9\x1dPW\xf3\x07\xd6\x06\xb5G\xf9\xd11\
-\xd6Q8\xdb>\xa6\xda\xf6\xd5Z|\xba\x82\xd8N\xfe\
-N\x1f\xefd\xd0z\x01\xd4\xcec\x93/\xad\x09\xa8t\
-\xd6\x22y\x13M\x0dD\xfd||`\xf37\x91\xad\xd1\
-\xb1\x86\xef\x8b137t\x19\xeb\x1f\xec\xc8n!\xb9\
-\x1e\x1a\xb93\x17\x92m\xa6\x95\xbf-\xccZ(;\x99\
-\xf0\xce\xfb\xd8+\x98\xde3\xe3\xf3TS\xf7\xbe\xd9\xd8\
-L\x08\xcb\xf9\x92D\x97\x0d/\xc2~k}m?\x96\
-\xbb)w[\xfc\xb1\xba,_%5\x99y\x98B\xbf\
-\x9cg\xdbF\xa1\xb5\x82\xae\x14\xceS\xe8}(\xdc\xa9\
-\xa0\x89\xff\xc0\xff\xff\x02\x19\x1fs\xfa\
+\x001\xf5x\xda\xcd\x1ako\xdb8\xf2\xfb\x02\xfb\x1f\
+x\xfe\xb0\xb1osr\x1a\xa0\xc0\xc1\xbb9 \xb1\xd3\
+&@\x92f\xebl\x8bCQ\x14\xb4D\xdbDhQ\
+G\xd1q\xb2\xad\xff\xfb\xcd\x90\x92E=-;\xe9\xf5\
+\xdc\xc2\xb1\xc8\x19r^\x9c\x17\xc5\x17\x91T\x9a\xfc\xa1\
+\xffXr\xff\xfe\xe7\x9fx\xee\xd9\xfb\xc8\xc3@\xaeJ\
+\xc3C\x19j%E\x5c;\xe1]S\xcd\x14\xa7\xa2\x04\
+qE\x9f\xe4R\x971G\x00,g0\x8e\xff\xec\xb6\
+\xe4\xeb\xcf?\x11\xf8\xf0`@V\x09!\xf8\xbc\xe2\x81\
+\x9e\x0f\xc8\xabWGGv`\xce\xf8l\xae\x07\xe4\x9f\
+8`\x87\x86\x12\x96\x0fY\xa8=\x19\xe2o\xc14\x83\
+e\x92\x15\xf1\xf3HN\xc8\xd8W\x8c\x85\x9eY\x90\xf4\
+\xc91\xf9\x07\xd9\xfc\xce \x9f2H\xbbS\x02\x9a=\
+X\xd8u\xba\xb9\xe6Z\xb0\x01\xf9O|\xa7\xba\x9dS\
+\xa5\xd9\x82\xc7\x9d\x9e\x9d{\xe01\x9f\xe0\xacVKf\
+\x87\xec\xf7B\x06Tp\xfd4\x00\x91x\xa7Q$\xb8\
+O5\x97\xe15\x8e[\x90\xa9\xa0\xb3\xd8\xcc\x7ft\xc4\
+a\xbf\xfb}b\x07c`A\xcfA\xc6d\x19EL\
+\x91\x09U)D\x86\xffF\xd1\x05\x13,\x8e-\xce\x05\
+\x0fuJ|\xccg!\x15DH\x1a\x8c\xcd\xcf.L\
+&\xa3\x97A/\x07\x14\xcf\xe5\xeaV\xb1i\xb7<<\
+:[\xd0\x90\xce\x98\xaa\x98\x1bR}]9)#\x16\
+\xc2\xa6\xe7\x01\xd7Ruc\xadx8#\xfa)b\x87\
+\xe4\x81*\x84\xfa\x12Q \xfd\x90L\xa4\x14\x84\xc7_\
+B\xb6*\xaf?\x8e\xa8\xcf>2\xaa\xe7\xa5\x1d\xfc9\
+\xf3\xefG\x93?\xa3\x00\x0c4.\xd2\xa6\xa9\xd2#\xb9\
+\x0a\x91\xf9J\xe2F\x93\x11W\xcc\x07\xea\x9e\x0a\xb3@\
+\xc8h\x92\x92\x1c\x82t\xf3\xd3\xec\x11\xad=\x83\x88\x80\
+\xb8<\x84=\x0f%\x08\x0b\x13)\xd8]\xe9'#\x06\
+C]p\xc5\xe3\x8d\xce\xa6\xcb\xd0G[!\x91\x8c\x96\
+\x028\xc3\xc9\xae]8\xc6\xdf=\xd7\xf23|0l\
+\x07(\x83\xd0\xecQ\xbf\xe1L\x04cF\x95?\xf7X\
+H\xc1f\x03\x00\xcf\xac\xd6\x183P\xe3/\x95\x82s\
+v\x19\x06\x0c\x8f\x94\x80\x85>p\xb6\xf2\xdc\xf1\x0c\x03\
+\x8cE\xb1xn\xe8\xebe\xc3`\xbdc\xa6\x09(,\
+\xbf\xde\x84\xfa\xf7\x84N\xc1\x97\xa4\x98\xc6\x22\x00L\xe4\
+\x08\xe6S\xd2\xcd!\xfe\xeb\x84\x1c\x91_~\xc9\xaf\xf6\
+\xbb\xc1\x82\x03\xc5\x84\xe7\xcbe\x98\x17\x8b\x11M\x15\xf5\
+\xc0U53\xeb\xc2\xb9\xdf\xa8!\xc7eN\xf4\xd9\xfe\
+\x02D\xeb\x8a`*\x15\xe9\xa2@9\xecw\xf4\x1b\xfc\
+\xf9\xdd\xd1\x94'X8\xd3s\x18\xfe\xf5\xd7\x12\xd5\x88\
+\x85&\x87\xe2\xdf`|\xe2\x9f=\x1c\xf4\xb4\xbc\x92+\
+\xa6\x864f\xee~)b\xc0b_\xf1\xc8\xd0]\xc4\
+w\xe6\xb6-\x13\x1bKA\x0b)\xd8\x0e>7!\xa3\
+\xea\x0c\x9d<\xf4\xc5\x12v\xec\xda\xa5z\xe4\xdb7\x97\
+\xb8\xd2|I\x0cy\x01S\xf0~a\xd0\xcd1T\xd8\
+z]R\xa5!\x08\xfc\xf5pN\xc3\x99EK\xe9-\
+k\xb9\x04\xe8\x12de\x22\xc0W\xb0\xe0\x0b\x1c\xb2\xe4\
+hX\xdafLw+M\xad\x977\xe9\xdc\x02\x7f;\
+9!K\x00\x9a\xf2\x90\x05e\xcb\xcd\xfcu\x9b}\xbc\
+\xf1\xe5\xdb/\x97\xa3\x82@\x188^\xbb\xc85\x0b\x97\
+\xb5\xc7~M\x98\x88Y\x91\x82Z\xe4)\xf8\x17\xd6\xe2\
+\xd4\x98#\xb1\xd3\x99\xc9y\xb2O\x9f\xeb\x1dX\x09\xb3\
+\xde\xc39\xd4\x96I\x14\xd2\xbfG\xee\xbaZ\xcef\x82\
+\xe5\xe8D\x85U\x0c\xe3\x07C\xc7\x1b.X\xb3`\x8c\
+\x10M\x9ch\x07\x0b!\xa7\x8d\xbcS\xdd@\xd4e3\
+\x08\x5c[\xb4S\xa9\xdb\x1a\x06\xf2f\xd1H\x7f\x19\xb4\
+\x8e\xfc\x8aE\xeb\xa9/\x18f\x9d\xde&Rk\xb9\xb8\
+\x0c\xa7\xf2\x0c\xec`\x01\x89\x0fd\x1f\x87$\xf9q\x07\
+\x19FNg\x19\xf8\x15\x9d\x80\xe1\xa1\xb5\xc0f\x09x\
+\x06\x17C\xaa\x05N\xaf[\xb7\x8e\x0f\xfe\x8etVT\
+\x85\x10\xb6:\x83<W\xc5M|) \x04\x9c\x904\
+u\xb6\x03\xdd\xcd\xe3{\x16\x14\x8e\xebD1z_\xdc\
+\x8d\xc3\x82;o\x05\xc1\x87\xcd\x14\x84\xc4\xa0q\x87Z\
+\xf9\xa2}\xbce!ST\xd8D\xde\x15\xc9\xa1M\x88\
+7\xe2\xce\x89(0\xe0\x09\xae\xe7`e\xf2\xc6\xa7:\
+\x04\xb32\xda\x01\xfe\xdd\xb2j\x95\x06\xf3\x80\xc8F\xbd\
+\xbf7\x09\xa0\x81OsD\xc8\xd6v\xe33Cl\xcf\
+\xaa\x83\xd3\xc8my\xedz\x86\x1d\xd8]yN\xea\x99\
+=\x19O\xb0w\xe7>El%\x82\xc2.\xdb\xe5\x90\
+\x22T\x0a\xc3\xf2\x7f\x9d\xac\xe5z|\xa8(\x8b\xf2\xcc\
+fMA\x97\xd6y\x8e\xcf\xd04\x0c\xa8\x0a\xce\x96p\
+&C(\xc7\xec\xf2\xde\x90\x86>\x13\xe4[\xfa\xfco\
+\x16\xbbh2<\xf5}\x16\x15\xab\xd8d\xc5\xaa\xb2\xa5\
+\xea\xc0\xee\xc0J\x22\x92\x1f\xc0\x0f\xd4\xa7\xa8\x87?\x95\
+8\x7f\x04\xff\x04\x11B<u;s\xad\xa3x\xd0\xef\
+\xcf\xa0\xba]N\xc0\x85-\xfa\xa7\x90d\x7f\x80Y\xf6\
+\xd4/T\xd9{\xf2\x9e\xf8\x81\xbdy~w_\xb9g\
+\xf5f7X1\xba\xab>\x0eH\x17\xca[lZ\xd8\
+.D\xd2\x8d\xe8\x15\xda\x11\x19X\xd2\x81H[\x11\x09\
+`\x0d\xf9\x9b0!d\xccn\xa5\xe0>\xact\x0b\x05\
+c\xe4\xdd\xc8\xd3\xa5\x96C\x9ch\xc7f\xa6V\xabf\
+\x17k(\xc5r\x11\xda>OQ\xb5\x00<\x97*\xf6\
+\xa6\x5c\x00]\x96\x8b<\x84\x09PU\x99=\xc6\xe0\xb4\
+\xa7r\x1ebE\x88e\xa0){\xe4\xd4\xfef+\x02\
+e=\x9d@\x1c\x1ctz\xe5%,M\x9e\x8d\x86\xd7\
+T\xcdx8 \xaf^\x97\x01\xa7\x12\x84\x1bI\x1eB\
+B\xfb\x17\x03\x98\xe3\xda\xc2\x01?wi2YE7\
+*\xdc4\x07n\x80\xd4Z\x9aP \x1fm_\xab\x9c\
+\xff\x98\x0e\x80\xa0>\x9bK\x01\x07\xfc\xce\x91\x04.\xda\
+i,kZ\x1d9\xdb\xbd\xd8\x90i\x12\x9e\x86\xa3\x84\
+\xf9]\xb5i'}\x0e3\xe7d\xdb\xb6!\xd6\xb9\x85\
+4\x1cr\x14\xb0\x01\xb04BIL\x1f\x18\x08\x1b\x99\
+\xf2<\xaf\xe3\x14\xc3\x98@B\xce?p\xb6\xf2\xc6\x00\
+\x8d\x8f\x19\x18j\x1fF\xc0\x18\xc08?uN!\x93\
+\xe7\xb0b\xd7\x03\x8f\xd8\xeb|n\xc9|\xd6\x9bI\xca\
+-\xdcd/\xee\x93\x1eNK\xee\x1f\xa8\xe0\x01\x01R\
+\xbd\xd9_\x84Z\xda[\x88\xe1]\x92\x8bo\x17\xc3\xdf\
+w\x93C\xd6\x81j'\x87\xd3I\xe1\x84\xa3\x08(\x0e\
+\xba\x12\xd8\x80\xdf\x16\xdcn\xb3/\x10l\xaaoi\x10\
+@\xe2< \xce\x11U\xe8\xe8\xaa&\xec\xa9vg\xb2\
+\xb99\xc3\xc08 X<@\x01Pb\x1c\xe8\xd62\
+:\xc3Ni~\x06\x11*\x9d\x91\xdb\xe1E1uz\
+E\xd4\x14\xfd\x12\x02S\xd5\x12\x99O\xeb@$ \xa3\
+\xc4q\xe5\x0d W\x83\x85C\xf0\xd9\xf7FqN\x04\
+Ir\x96u\x19i\xfd\x0c\x92\xae \x03\xd8\x91\xa6B\
+\xd7w\x17\x82\xc6\x0c\xdb\xba\x1aj\x92\xaf{\x11\x9d\x9d\
+\xbd\xb4\xf2ld\xee\xd2^=\xa4\xec\xb5\xe0\xcd=\xd8\
+/,\xf0\xcck\xb6\xa2\xfd\xfcqW\xda]\x97\x5cO\
+\xbbq\x84\xb6\xac\x1eTu\x10\xbe\xbb\xfe\x8aE~\xb3\
+\x14\x00\x98\xdc\xd1Y\xdc\xd2.\xdd\x1b\x87\xffG\xe6\xdd\
+\xfeJ#\xe3\xe8\xfc7\xca'oL\xe8l!\x83\xd2\
+\xdd\xc5K\x0b\xa1\x85K\xc1\x8b\x22\x06N\xdegm\xb5\
+f\xaf\x96^\xd8\xb3\x9d?r\xddb\x7f{\xe1\xe8\x99\
+d\xb9\x1d\x0d\xeb\xb6\xc1\x03\x15\x1eo:_[\x82\x8b\
+m\x91\xed\x17^\x92\xd4s\xdc\xb0W\x1b\xa5\xe7#U\
+\xabX\x90\xbf\xc8;\xb0\x14\x1c\x1c\x92O\x9f\x0fM~\
+\xfb\xb2\xde3\xd7x~6\x8fH\xb4\xf7l.\xcd>\
+5l\xeee5y\xab\xc0\x1bN\x92\x5cq>+\xf7\
+\x18\xe2\x85(y\xcf0F\xb4`\xb9\x1a\xc2\x14\x8b5\
+\x17\xaf[\xf8\x7f\x8eH6\xf9f\x8bst\xc1D\xf4\
+\x02\x82\xc2[\xba\xe4\xf2\xb8\x85\xb4\x8a\xb7\xcd\xff\xcb\x9c\
+(!{$\xfd\xe5\x022k\xf3.A\x0b\x92+\x9b\
+/\x07i\xf3%\xeb\xb8xI\x1f\x86\xcb\xb4\xf7r\xf0\
+\xd2\x9ez\x0c\x16\x05\x96i\xab\xa6\x1b\xd9N\xe4\xcd\xf4\
+76\x8f\xfa\x13!'\xfd\x05\x8d\x01\xaf?\xbc8\xbd\
+y{~\xf5\xee\xad\xb7\x08\x0e~\x84\xe2\xec\x91$\x97\
+q\xbcd\xdf\x99o\x8e{\xc4?\x84KSC>\xcb\
+\xef8\x05g\xae}\xbb\x97\xdf\xa9d{*\xc1\xf6\xa0\
+z\xaclM\xa1\x0f*\x5c\xaf\xe4\x01l/\x89?2\
+Q\xddK\xaa+t\x1b\x8b\xdd\xea\x82\xb7\x92\xfa\xf7r\
+\xb5o/.\x06wn\xd6>>*\xaa\xb8\xa9\xc9\xe7\
+\xb4\xb5\x16\xf4\x91/\x96\x8b\xa4\xb3u\xfc\xfa\xa8\xcaT\
+\x1a\xdbg\x9b\xfa<\x7fa\xbb\x7f\x94o\xdbps`\
+\xb5\x8c6\xdd\xc2\xa3j\xc8\x9a\xf6\x9c%\xb6\xd3\xab3\
+p\x84M^!h4\xf2\x9a\xd7f\xb6\xdbwy0\
+g\x0f5l'\xaf\x0c4Q\x84:I_-\xa8\x87\
+\xdaE\xd8\x05\xf8\x8b\xe4\xa5\xbef\x849\x00\x09\x04\xbc\
+\x96\x0fl\xb4T&\xcc\x0d\xc8Q=\x86/x\xb4m\
+\xd5)\x04\xcdx\x1b\xd0\xd8WR\x883\xaa\xbc\x07\xa6\
+4\xf7\xb1\xd3\xbey\xd9\xae\x91\xd6\x01D5_\x83\xd6\
+\x05#_\x89\xb9\x8c\x1ddw\xb1\xd4\xf7\xe1$\xfeF\
+\x14\x0d8R\xf1\x9a\xac\xeb\xd7\x04\x1f\xe9\xbc\xd6\x91\x99\
+R\xc5\xcb)\x0d\xab\x04\x10dg\xb0\xff\x804\xb9\xec\
+\xa2\xeaG\x09V3t\xf2\xb6fjS\xde\x03~\x99\
+\xc1f\xbc\xf4\xa5\xce\xe3\xa3f\xb8\xc4+\xdbx\x82\x9d\
+\xd0u3\xfc\xb5\x5c\xc6\x0c\x82 \xdd\xc6f;\x1f\xb9\
+%p\xd5\xbdP\xc6\xf3o\x92\xb5<\xd0-\xa6\x16\xf8\
+\xa6\x8c\x15\xb7yi\xa6\xad2\x0d\xf0\xce;\xae\xeb\x5c\
+\xc8\xe6pl\xf3!\x8d'fgw@}\xcd\x1fX\
+\x13\xd4\x1e\xe5G\xcbXG\xe1l\x87\x98j\xdb7x\
+\xf1\xe9\x0ab;\xf9\xb6y\xbc\x93Q\xe3\x05P3\x8f\
+u\xbe\xb4\x22\xa0\xd2I\x83\xe4M45\x10\xd5\xf3\xc9\
+\x81uo\x22\x1b\xa3c\x05\xdf\x17Cfn\xe82\xd6\
+?\xd8\x91\xddBr54rg.$\x9bL\xcb\xbd\
+-\xccZ(;\x99\xf0\xce\xfb\xd8+\x98\xce3\xe3\xf3\
+XS\xff\xbe\xde\xd8L\x08s|I\xaa\xcb\x9a\xf7m\
+\xbf\xb7\xbe\xb6\x1f\xcb\xdd\x94\xbb-\xfeX]\x16\xaf\x92\
+\xea\xcc<\xde@\xbf\x9cg\xdbF\xa1\xb5\x82\xb6\x14N\
+7\xd0\xfbP\xb8SA\x93\xfc\x81\xff\xff\x05|\xb6\x97\
+\x22\
 \x00\x00\x08k\
 \x00\
 \x00r]x\xda\xed\x5cos\x9b8\x1e~\xdf\x99~\
@@ -7192,47 +7194,47 @@ qt_resource_struct = b"\
 \x00\x00\x01\x8f\xc8\xd7c\x16\
 \x00\x00\x01\x0e\x00\x00\x00\x00\x00\x01\x00\x01\x16\xe5\
 \x00\x00\x01\x8f\xc8\xd7c\x16\
-\x00\x00\x03\x8c\x00\x01\x00\x00\x00\x01\x00\x01N.\
+\x00\x00\x03\x8c\x00\x01\x00\x00\x00\x01\x00\x01ND\
 \x00\x00\x01\x8f\xcf\xcd\xa1A\
-\x00\x00\x03\xfe\x00\x01\x00\x00\x00\x01\x00\x01X\x22\
+\x00\x00\x03\xfe\x00\x01\x00\x00\x00\x01\x00\x01X8\
 \x00\x00\x01\x8f\xcb\x85\x0fz\
-\x00\x00\x03\xc2\x00\x01\x00\x00\x00\x01\x00\x01QJ\
+\x00\x00\x03\xc2\x00\x01\x00\x00\x00\x01\x00\x01Q`\
 \x00\x00\x01\x8f\xcf\x1c*%\
 \x00\x00\x02\xee\x00\x01\x00\x00\x00\x01\x00\x016{\
 \x00\x00\x01\x8f\xcb\x84\x8f\xb2\
 \x00\x00\x03\x18\x00\x01\x00\x00\x00\x01\x00\x01;\xcf\
-\x00\x00\x01\x8f\xcb\x849\xd5\
-\x00\x00\x05\x84\x00\x00\x00\x00\x00\x01\x00\x01\x9b\x17\
+\x00\x00\x01\x8f\xe4[\x11\xaf\
+\x00\x00\x05\x84\x00\x00\x00\x00\x00\x01\x00\x01\x9b-\
 \x00\x00\x01\x8f\xcb\x83\x89\xfd\
-\x00\x00\x03N\x00\x01\x00\x00\x00\x01\x00\x01E\xbf\
+\x00\x00\x03N\x00\x01\x00\x00\x00\x01\x00\x01E\xd5\
 \x00\x00\x01\x8f\xcb\x86T\x18\
-\x00\x00\x05b\x00\x00\x00\x00\x00\x01\x00\x01\x95h\
+\x00\x00\x05b\x00\x00\x00\x00\x00\x01\x00\x01\x95~\
 \x00\x00\x01\x8f\xc8\xd7c\x19\
 \x00\x00\x034\x00\x02\x00\x00\x00\x04\x00\x00\x00*\
 \x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x04\xa0\x00\x01\x00\x00\x00\x01\x00\x01r\xe9\
+\x00\x00\x04\xa0\x00\x01\x00\x00\x00\x01\x00\x01r\xff\
 \x00\x00\x01\x8f\xc8\xd7c\x19\
-\x00\x00\x05@\x00\x01\x00\x00\x00\x01\x00\x01\x8b\x8a\
+\x00\x00\x05@\x00\x01\x00\x00\x00\x01\x00\x01\x8b\xa0\
 \x00\x00\x01\x8f\xc8\xd7c\x19\
-\x00\x00\x04\xc2\x00\x01\x00\x00\x00\x01\x00\x01{\xcc\
+\x00\x00\x04\xc2\x00\x01\x00\x00\x00\x01\x00\x01{\xe2\
 \x00\x00\x01\x8f\xcb\x83z\xd6\
-\x00\x00\x04$\x00\x01\x00\x00\x00\x01\x00\x01^\xa5\
+\x00\x00\x04$\x00\x01\x00\x00\x00\x01\x00\x01^\xbb\
 \x00\x00\x01\x8f\xd5{\x7f~\
-\x00\x00\x04R\x00\x00\x00\x00\x00\x01\x00\x01j\xf6\
+\x00\x00\x04R\x00\x00\x00\x00\x00\x01\x00\x01k\x0c\
 \x00\x00\x01\x8f\xc8\xd7c\x19\
-\x00\x00\x04z\x00\x01\x00\x00\x00\x01\x00\x01p\xbb\
+\x00\x00\x04z\x00\x01\x00\x00\x00\x01\x00\x01p\xd1\
 \x00\x00\x01\x8f\xd301\xb9\
-\x00\x00\x05\x1c\x00\x01\x00\x00\x00\x01\x00\x01\x87\x96\
+\x00\x00\x05\x1c\x00\x01\x00\x00\x00\x01\x00\x01\x87\xac\
 \x00\x00\x01\x8f\xcb\x85\x05&\
-\x00\x00\x04\xe2\x00\x00\x00\x00\x00\x01\x00\x01\x82m\
+\x00\x00\x04\xe2\x00\x00\x00\x00\x00\x01\x00\x01\x82\x83\
 \x00\x00\x01\x8f\xd3.\xfb<\
-\x00\x00\x05\xb8\x00\x01\x00\x00\x00\x01\x00\x01\xa3\xca\
+\x00\x00\x05\xb8\x00\x01\x00\x00\x00\x01\x00\x01\xa3\xe0\
 \x00\x00\x01\x8f\xc8\xd7c\x19\
-\x00\x00\x06\x00\x00\x01\x00\x00\x00\x01\x00\x01\xa9\xc7\
+\x00\x00\x06\x00\x00\x01\x00\x00\x00\x01\x00\x01\xa9\xdd\
 \x00\x00\x01\x8f\xc8\xd7c\x19\
-\x00\x00\x05\x9c\x00\x01\x00\x00\x00\x01\x00\x01\xa0\xc7\
+\x00\x00\x05\x9c\x00\x01\x00\x00\x00\x01\x00\x01\xa0\xdd\
 \x00\x00\x01\x8f\xc8\xd7c\x19\
-\x00\x00\x05\xdc\x00\x01\x00\x00\x00\x01\x00\x01\xa6\x89\
+\x00\x00\x05\xdc\x00\x01\x00\x00\x00\x01\x00\x01\xa6\x9f\
 \x00\x00\x01\x8f\xc8\xd7c\x19\
 "
 

ui/Artemis.qml
@@ -55,8 +55,9 @@ Window {
         listModel.clear()
         for (var i = 0; i < loadedList.length; i++) {
             var name = loadedList[i].name.toLowerCase()
+            var description = loadedList[i].description.toLowerCase()
             var search = textFieldSearch.text.toLowerCase()
-            if (name.includes(search)) {
+            if (name.includes(search) || description.includes(search)) {
                 listModel.append(loadedList[i])
             }
         }

commit 7929c23ac536dd510e3436e4d671160b174dd41e

Fixed a bug introduced in 10607c88ea505e1fd4fa2160aaffa7c324ea9c61

artemis/utils/constants.py
@@ -65,7 +65,7 @@ class Query():
     
     SELECT_ALL_LOCATION = "SELECT DISTINCT VALUE FROM location ORDER BY VALUE ASC"
     
-    SELECT_SIG_ID = "SELECT SIG_ID, NAME FROM signals WHERE SIG_ID IN ({}) ORDER BY NAME ASC"
+    SELECT_SIG_ID = "SELECT SIG_ID, NAME, DESCRIPTION FROM signals WHERE SIG_ID IN ({}) ORDER BY NAME ASC"
 
     SELECT_ALL_CAT_LABELS = "SELECT CLB_ID, VALUE FROM category_label ORDER BY VALUE ASC"
     

artemis/utils/sql_utils.py
@@ -132,7 +132,7 @@ class ArtemisDatabase(Database):
         sig_ids = ",".join(str(num[0]) for num in matching_sig_ids)
 
         self.all_signals = self.execute(Query.SELECT_SIG_ID.format(sig_ids))
-        keys = ('SIG_ID', 'name')
+        keys = ('SIG_ID', 'name', 'description')
         result = [dict(zip(keys, values)) for values in self.all_signals]
         self.all_signals = result
 